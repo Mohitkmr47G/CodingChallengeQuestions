0766becc18c7bae080eae06a29e5c042f08eeffd
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-08-17 16:23:26 +0530

    Create Q1.js

---
+++ Q1.js
@@ -0,0 +1,8 @@
+// Call the function before declaration
+const result = addNumbers(5, 7);
+console.log("Result:", result);
+
+// Function declaration
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
