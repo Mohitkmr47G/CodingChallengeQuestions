10254141c10711fdf33a7c52754dd802a79c4950
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-08-17 16:23:56 +0530

    Create Q2.js

---
+++ Q2.js
@@ -0,0 +1,8 @@
+// Call the function expression before declaration
+const result = multiplyNumbers(3, 4);
+console.log("Result:", result);
+
+// Function expression
+const multiplyNumbers = function(num1, num2) {
+  return num1 * num2;
+};
